b4b6206db75638fc5f1f37f1186ca519cd9f928d
OmarMWarraich <earthling986@gmail.com>
2022-09-27 10:16:00 +0500

    feat: Create components and render TodoContainer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const element = <h1>Hello from Create React App</h1>;
+import TodoContainer from './components/TodoContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    { element }
+    <TodoContainer />
     ,
   </React.StrictMode>,
 );
