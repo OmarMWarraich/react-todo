c3980cb2aa1991b36b84f20877129b6526d6bdf2
OmarMWarraich <earthling986@gmail.com>
2022-09-27 16:57:50 +0500

    feat: Manage localStorage w/ react lifecycle methods

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,39 +8,31 @@ import InputTodo from './InputTodo';
 class TodoContainer extends React.Component {
     // eslint-disable-next-line react/state-in-constructor
     state = {
-      todos: [
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
-      ],
+      todos: [],
     };
 
-    handleChange = (id) => {
-      this.setState((prevState) => ({
+    componentDidMount() {
+      const temp = localStorage.getItem('todos');
+      const loadedTodos = JSON.parse(temp);
+      if (loadedTodos) {
+        this.setState({
+          todos: loadedTodos,
+        });
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      /* eslint-disable-next-line react/destructuring-assignment */
+      if (prevState.todos !== this.state.todos) {
         /* eslint-disable-next-line react/destructuring-assignment */
-        todos: prevState.todos.map((todo) => {
-          if (todo.id === id) {
-            return {
-              ...todo,
-              completed: !todo.completed,
-            };
-          }
-          return todo;
-        }),
-      }));
-    };
+        const temp = JSON.stringify(this.state.todos);
+        localStorage.setItem('todos', temp);
+      }
+    }
+
+    componentWillUnmount() {
+      console.log('Cleaning up...');
+    }
 
     delTodo = (id) => {
       this.setState({
